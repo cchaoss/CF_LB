e3defdd6bab593d23ca6c8329c772c19e752bc13
cchaoss <862281335@qq.com>
2016-11-16 14:37:54 +0800

    store

--- src/main/rx/rx.c
+++ src/main/rx/rx.c
@@ -609,7 +609,7 @@ void calculateRxChannelsAndUpdateFailsafe(uint32_t currentTime)
 			mspData.mspCmd |= ALTHOLD;//开定高
 			if(mspData.motor[THR] >= 1650)mspData.motor[THR] = 1550;
 				else if(mspData.motor[THR] >= 1490)mspData.motor[THR] = 1433;
-					else mspData.motor[THR] = 1360;
+					else mspData.motor[THR] = 1370;
 			
 			
 			if(flag.height <= 300){	
@@ -620,9 +620,9 @@ void calculateRxChannelsAndUpdateFailsafe(uint32_t currentTime)
 					mspData.motor[THR] = 1100;
 					mspData.mspCmd &= ~ARM;
 				}
-				else mspData.motor[THR] = 1360;
+				else mspData.motor[THR] = 1400;
 			}else b =0;
-		}
+		}else b = 0;
 #endif 
 
 		SetTX_Mode();
