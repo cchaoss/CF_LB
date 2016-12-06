97d66e9b697ca41a9965b2deff0fd568ecb3d8fa
cchaoss <862281335@qq.com>
2016-12-06 11:17:34 +0800

    普通油门+有头+信号丢失报警

--- src/main/drivers/nrf2401.c
+++ src/main/drivers/nrf2401.c
@@ -15,7 +15,7 @@
 #include "io/beeper.h"
 #include "build/debug.h"
 
-golbal_flag flag = {"EMT",101,0,0,0,0,0,0,0,0,true};
+golbal_flag flag = {"EMT",103,0,0,0,0,0,0,0,0,true};
 package_328p msp_328p;
 dataPackage mspData;
 dataPackage t_mspData;
